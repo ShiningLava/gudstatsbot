fc5ecd7d60d91512cb0bdd50a33e88f9d3081f53
ShiningLava <crgpwns@gmail.com>
2025-07-03 22:04:30 -0600

    created basic functionality for slash command. lots to cleanup next

--- main.py
+++ main.py
@@ -2,27 +2,65 @@ import discord
 from discord import app_commands
 import sqlite3
 import json
+from discord.ext import commands
 
 with open('config.json', 'r') as g:
     config = json.load(g)
 
 token = config['token']
-guild_id = config['guild_id']
+#guild_id = config['guild_id']
+guild_id = 120731559874527232
 target_user_1 = config['target_user_1']
-intents = discord.Intents.default()
 sqliteConnection = sqlite3.connect('pokebot.db')
 cursor = sqliteConnection.cursor()
 
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=discord.Object(id=guild_id))
-        await self.tree.sync(guild=discord.Object(id=guild_id))
-        print("Global slash commands updated for GudStatsBot")
+#@tree.command(
+#    name="echo",
+#    description="Echoes a message.",
+#    guild=discord.Object(id=guild_id)
+#)
+#async def echo(interaction):
+#    await interaction.response.send_message("echo")
+
+#class MyClient(discord.Client):
+#    def __init__(self, *, intents: discord.Intents):
+#        super().__init__(intents=intents)
+#        self.tree = app_commands.CommandTree(self)
+
+#    async def setup_hook(self):
+#        # This copies the global commands over to your guild.
+#        print("--0-0-0-0-0-0-0-0000-0---0-0-00-0--0-0--00---0-00-00-0-0-0-0-")
+#        self.tree.copy_global_to(guild=discord.Object(id=120731559874527232))
+#        await self.tree.sync(guild=discord.Object(id=120731559874527232))
+#        print("Global slash commands updated for GudStatsBot")
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    tree.copy_global_to(guild=discord.Object(id=120731559874527232))
+    await tree.sync(guild=discord.Object(id=120731559874527232))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.author.id == (int(target_user_1)):
+        print('Pokebot message detected')
+        cursor = sqliteConnection.cursor()
+        generate_pokebot_entry()
+
+@tree.command(
+    name="echo",
+    description="Echoes a message.",
+    guild=discord.Object(id=guild_id)
+)
+async def echo(interaction):
+    await interaction.response.send_message("echo")
 
 def add_pokebot_entry(conn, entry):
     sql = '''INSERT INTO pokebot_test(species,total_ivs,hp,attack,defense,special_attack,special_defense,speed,nature,shiny_value,held_item,phase_encounters,phase_same_pkmn_streak,receiving_user)
@@ -66,20 +104,27 @@ def generate_pokebot_entry():
         print("error opening database", e)
 
 def main():
-    client = MyClient(intents=intents)
 
-    @client.event
-    async def on_ready():
-        print(f'Logged in as {client.user} (ID: {client.user.id})')
+#    @client.event
+#    async def on_ready():
+#        print(f'Logged in as {client.user} (ID: {client.user.id})')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        if message.author.id == (int(target_user_1)):
-            print('Pokebot message detected')
-            cursor = sqliteConnection.cursor()
-        generate_pokebot_entry()
+#    @client.event
+#    async def on_message(message):
+#        if message.author == client.user:
+#            return
+#        if message.author.id == (int(target_user_1)):
+#            print('Pokebot message detected')
+#            cursor = sqliteConnection.cursor()
+#            generate_pokebot_entry()
+
+#    @tree.command(
+#        name="echo",
+#        description="Echoes a message.",
+#        guild=discord.Object(id=guild_id)
+#    )
+#    async def echo(interaction):
+#        await interaction.response.send_message("echo")
 
     client.run(token)
 
