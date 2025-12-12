2d4a31f2c9598c7eb81d0289b34dbc381b85a139
ShiningLava <crgpwns@gmail.com>
2025-12-12 05:25:31 +0000

    Added functionality for other operators for the sql query for the custom_database_Search

--- main.py
+++ main.py
@@ -92,15 +92,23 @@ async def lowest_iv(interaction):
 
 
 @tree.command()
-async def custom_db_search(inter: discord.Interaction, species: str | None,
+async def custom_db_search(inter: discord.Interaction, 
+			  species: str | None,
 			  total_ivs: str | None,
 			  shiny_value: str | None,
-			  held_item: str | None,
+			  held_item: str | None, held_item_operator: str | None,
 			  phase_encounters: str | None,
 			  phase_same_pkmn_streak: str | None,
 			  receiving_user: str | None,
 			  message_id: str | None,
-			  order_by: str | None):
+			  order_by: str | None,
+                          species_operator: str = '=',
+                          total_ivs_operator: str = '=',
+			  shiny_value_operator: str = '=',
+			  phase_encounters_operator: str = '=',
+			  phase_same_pkmn_streak_operator: str = '=',
+			  receiving_user_operator: str = '=',
+			  message_id_operator: str = '=',):
     """
     Custom database search
 
@@ -111,14 +119,40 @@ async def custom_db_search(inter: discord.Interaction, species: str | None,
     column: str
         The column to echo
     """
-    arguments_dict = {"species": species, "total_ivs": total_ivs, "held_item": held_item,
-		     "shiny_value": shiny_value, "phase_encounters": phase_encounters,
-		     "phase_same_pkmn_streak": phase_same_pkmn_streak, 
-		     "receiving_user": receiving_user, "order_by": order_by}
+
+
+    #species_operator = "="
+    #total_ivs_operator = "="
+    #shiny_value_operator = "="
+    #held_item_operator = "="
+    #phase_encounters_operator = "="
+    #phase_same_pkmn_streak_operator = "="
+    #receiving_user_operator = "="
+    #message_id_operator = "="
+
+    arguments_dict = {"species": species,
+		     "total_ivs": total_ivs,
+		     "held_item": held_item,
+		     "shiny_value": shiny_value,
+		     "phase_encounters": phase_encounters,
+		     "phase_same_pkmn_streak": phase_same_pkmn_streak,
+		     "receiving_user": receiving_user,
+		     "order_by": order_by}
+
+    arguments_operators_dict = {"species_operator": species_operator,
+				"total_ivs_operator": total_ivs_operator,
+				"held_item_operator": held_item_operator,
+				"shiny_value_operator": shiny_value_operator,
+				"phase_encounters_operator": phase_encounters_operator,
+				"phase_same_pkmn_streat_operator": phase_same_pkmn_streak_operator,
+				"receiving_user_operator": receiving_user_operator}
+
     user_sql_options_list = []
     for key in arguments_dict:
         if not arguments_dict[key] == None:
-            user_sql_options_list.append(f"{key} = '{arguments_dict[key]}'")
+            key_operator_string = f"{key}_operator"
+            key_operator = arguments_operators_dict[key_operator_string]
+            user_sql_options_list.append(f"{key} {key_operator} '{arguments_dict[key]}'")
     sql_list_formatted_1 = ' AND '.join(map(str, user_sql_options_list))
     #sql_list_formatted_2 = sql_list_formatted_1.replace(' ', 'AND')
     user_where_string = f"WHERE {sql_list_formatted_1}"
@@ -145,7 +179,10 @@ async def custom_db_search(inter: discord.Interaction, species: str | None,
 #				     receiving__user,
 #				     message_id)
 
-    await inter.response.send_message(records)
+    try:
+        await inter.response.send_message(records)
+    except:
+        await inter.response.send_message("Database Error")
 
 @custom_db_search.autocomplete("total_ivs")
 async def column_autocomplete(interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
